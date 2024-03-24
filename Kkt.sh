50448d97579fb41d3945ddec8918a4e453bf9200
Inhuterk <158720380+Inhuterk@users.noreply.github.com>
2024-03-24 20:01:27 +0530

    Update and rename Lkt.sh to Kkt.sh

--- Kkt.sh
+++ Kkt.sh
@@ -1,8 +1,4 @@
-#!/bin/sh
-
-USER="test"
-PASSWORD="test"
-IP="192.46.211.32"
+#!/bin/bash
 
 random() {
     tr </dev/urandom -dc A-Za-z0-9 | head -c5
@@ -22,14 +18,14 @@ install_3proxy() {
     echo "installing 3proxy"
     URL="https://github.com/z3APA3A/3proxy/archive/3proxy-0.8.6.tar.gz"
     wget -qO- $URL | bsdtar -xvf-
-    cd 3proxy-3proxy-0.8.6
+    cd 3proxy-3proxy-0.8.6 || exit
     make -f Makefile.Linux
     mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
     cp src/3proxy /usr/local/etc/3proxy/bin/
     cp ./scripts/rc.d/proxy.sh /etc/init.d/3proxy
     chmod +x /etc/init.d/3proxy
     chkconfig 3proxy on
-    cd $WORKDIR
+    cd "$WORKDIR" || exit
 }
 
 gen_3proxy() {
@@ -48,16 +44,27 @@ stacksize 6291456
 flush
 auth strong
 
-users ${USER}:CL:${PASSWORD}
-
 $(gen_data)
+
 EOF
+}
+
+gen_data() {
+    for ((i=0; i<$COUNT; i++)); do
+        echo "Ram:CL:Ram $IP4/$((FIRST_PORT+i))/$(gen64 $IP6)"
+    done
 }
 
 gen_proxy_file_for_user() {
     cat >proxy.txt <<EOF
-${USER}:${PASSWORD}:${IP}
+$(gen_proxy_data)
 EOF
+}
+
+gen_proxy_data() {
+    for ((i=0; i<$COUNT; i++)); do
+        echo "$IP4:$((FIRST_PORT+i)):Ram:Ram"
+    done
 }
 
 upload_proxy() {
@@ -67,24 +74,6 @@ upload_proxy() {
     echo "Password: ${PASS}"
 }
 
-gen_data() {
-    seq $FIRST_PORT $LAST_PORT | while read port; do
-        echo "${USER}/${PASSWORD}/${IP}/$port/$(gen64 $IP)"
-    done
-}
-
-gen_iptables() {
-    cat <<EOF
-$(awk -F "/" '{print "ip6tables -I INPUT -p tcp --dport " $4 " -m state --state NEW -j ACCEPT"}' ${WORKDATA}) 
-EOF
-}
-
-gen_ifconfig() {
-    cat <<EOF
-$(awk -F "/" '{print "ifconfig eth0 inet6 add " $5 "/64"}' ${WORKDATA})
-EOF
-}
-
 echo "installing apps"
 yum -y install gcc net-tools bsdtar zip >/dev/null
 
@@ -92,15 +81,19 @@ install_3proxy
 
 echo "working folder = /home/proxy-installer"
 WORKDIR="/home/proxy-installer"
-WORKDATA="${WORKDIR}/data.txt"  # Changed to data.txt
+WORKDATA="${WORKDIR}/data.txt"
+mkdir "$WORKDIR" && cd "$_" || exit
 
-echo "Internal IP = ${IP}. External subnet for IPv6 = ${IP6}"
+IP4="192.46.211.32"
+IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 
-echo "How many proxies do you want to create? Example: 500"
-read COUNT
+echo "Internal ip = ${IP4}. External sub for ip6 = ${IP6}"
+
+read -p "How many proxies do you want to create? Example: 500 " COUNT
 
 FIRST_PORT=22000
-LAST_PORT=22099
+
+echo "Generating $COUNT proxies..."
 
 gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
 
