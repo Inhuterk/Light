de57cb7811f772a30c9217f318cb20c12d05207b
Inhuterk <158720380+Inhuterk@users.noreply.github.com>
2024-03-24 15:19:24 +0530

    Update parrot.sh

--- parrot.sh
+++ parrot.sh
@@ -1,31 +1,31 @@
-#!/bin/bash
+#!/bin/sh
 
 random() {
-    tr </dev/urandom -dc A-Za-z0-9 | head -c5
-    echo
+	tr </dev/urandom -dc A-Za-z0-9 | head -c5
+	echo
 }
 
 array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
 
 gen64() {
-    ip64() {
-        echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
-    }
-    echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
+	ip64() {
+		echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
+	}
+	echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
 }
 
 install_3proxy() {
-    echo "Installing 3proxy..."
+    echo "installing 3proxy"
     URL="https://github.com/z3APA3A/3proxy/archive/3proxy-0.8.6.tar.gz"
-    wget -qO- $URL | tar -xzvf -
-    cd 3proxy-3proxy-0.8.6 || exit
+    wget -qO- $URL | bsdtar -xvf-
+    cd 3proxy-3proxy-0.8.6
     make -f Makefile.Linux
     mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
     cp src/3proxy /usr/local/etc/3proxy/bin/
     cp ./scripts/rc.d/proxy.sh /etc/init.d/3proxy
     chmod +x /etc/init.d/3proxy
     chkconfig 3proxy on
-    cd "$WORKDIR" || exit
+    cd $WORKDIR
 }
 
 gen_3proxy() {
@@ -42,9 +42,12 @@ setgid 65535
 setuid 65535
 stacksize 6291456 
 flush
-auth none
+#auth strong
 
-$(awk -F "/" '{print "allow * \n" \
+users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' "${WORKDATA}")
+
+$(awk -F "/" '{print "auth strong\n" \
+"allow " $1 "\n" \
 "proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
 "flush\n"}' "${WORKDATA}")
 EOF
@@ -52,65 +55,66 @@ EOF
 
 gen_proxy_file_for_user() {
     cat >proxy.txt <<EOF
-$(awk -F "/" '{print $3 ":" $4}' "${WORKDATA}")
+$(awk -F "/" '{print $3 ":" $4 ":" $1 ":" $2 }' ${WORKDATA})
 EOF
 }
 
 upload_proxy() {
-    echo "Proxy is ready! Format IP:PORT"
+    local PASS=$(random)
+    zip --password $PASS proxy.zip proxy.txt
+    echo "Proxy is ready! Format IP:PORT:LOGIN:PASS"
+    echo "Password: ${PASS}"
 }
 
 gen_data() {
-    seq "$FIRST_PORT" "$LAST_PORT" | while read -r port; do
-        echo "/$IP4/$port/$(gen64 "$IP6")"
+    seq $FIRST_PORT $LAST_PORT | while read port; do
+        echo "$IP4/$port/$(gen64 $IP6)"
     done
 }
 
 gen_iptables() {
-    while read -r line; do
-        port=$(echo "$line" | awk -F "/" '{print $4}')
-        ip6tables -I INPUT -p tcp --dport "$port" -j ACCEPT
-    done < "${WORKDATA}"
+    cat <<EOF
+$(awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $2 "  -m state --state NEW -j ACCEPT"}' ${WORKDATA}) 
+EOF
 }
 
 gen_ifconfig() {
-    while read -r line; do
-        ipv6=$(echo "$line" | awk -F "/" '{print $5}')
-        ifconfig eth0 inet6 add "$ipv6/64"
-    done < "${WORKDATA}"
+    cat <<EOF
+$(awk -F "/" '{print "ifconfig eth0 inet6 add " $3 "/64"}' ${WORKDATA})
+EOF
 }
 
-echo "Installing necessary packages..."
+echo "installing apps"
 yum -y install gcc net-tools bsdtar zip >/dev/null
 
 install_3proxy
 
-echo "Setting up the working folder..."
+echo "working folder = /home/proxy-installer"
 WORKDIR="/home/proxy-installer"
 WORKDATA="${WORKDIR}/data.txt"
-mkdir "$WORKDIR" && cd "$_" || exit
+mkdir $WORKDIR && cd $_
 
-IP4=$(hostname -I | awk '{print $1}')
+IP4=$(curl -4 -s icanhazip.com)
 IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 
-echo "Internal IP = ${IP4}. External sub for IP6 = ${IP6}"
+echo "Internal ip = ${IP4}. External sub for ip6 = ${IP6}"
 
-echo "How many proxies do you want to create? Example 500"
-read -r COUNT
+echo "How many proxy do you want to create? Example 500"
+read COUNT
 
 FIRST_PORT=22000
 LAST_PORT=22099
 
-gen_data >"$WORKDIR/data.txt"
-gen_iptables >"$WORKDIR/boot_iptables.sh"
-gen_ifconfig >"$WORKDIR/boot_ifconfig.sh"
-chmod +x "${WORKDIR}/boot_*.sh" /etc/rc.local
+gen_data >$WORKDIR/data.txt
+gen_iptables >$WORKDIR/boot_iptables.sh
+gen_ifconfig >$WORKDIR/boot_ifconfig.sh
+chmod +x ${WORKDIR}/boot_*.sh /etc/rc.local
 
 gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
 
 cat >>/etc/rc.local <<EOF
-bash "${WORKDIR}/boot_iptables.sh"
-bash "${WORKDIR}/boot_ifconfig.sh"
+bash ${WORKDIR}/boot_iptables.sh
+bash ${WORKDIR}/boot_ifconfig.sh
 ulimit -n 10048
 systemctl start 3proxy
 EOF
