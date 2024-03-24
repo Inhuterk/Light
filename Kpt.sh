ab8ddd8395c506787c3aa73000af13c6baa45c82
Inhuterk <158720380+Inhuterk@users.noreply.github.com>
2024-03-24 20:57:55 +0530

    Update Kpt.sh

--- Kpt.sh
+++ Kpt.sh
@@ -66,7 +66,7 @@ upload_proxy() {
 
 gen_data() {
     seq $FIRST_PORT $LAST_PORT | while read port; do
-        echo "$(test)/$(test)/$IP4/$port/$(gen64 $IP6)"
+        echo "$/$/$IP4/$port/$(gen64 $IP6)"
     done
 }
 
